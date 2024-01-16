0d01e2ee78304de61b9867b09f9e370d97585d96
DESKTOP-6CB41U6\thbb2 <thbb268@live.com>
2024-01-16 16:58:36 +0800

    init project

---
+++ src/main/java/top/honosv/anigrab/AniGrabApplication.java
@@ -0,0 +1,13 @@
+package top.honosv.anigrab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniGrabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AniGrabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/honosv/anigrab/AniGrabApplicationTests.java
@@ -0,0 +1,13 @@
+package top.honosv.anigrab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniGrabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
